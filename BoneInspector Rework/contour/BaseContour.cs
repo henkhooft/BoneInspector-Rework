497c5d90e1f6743f570917c524d8c1560e150164
Rutger Hendriks <rutger_dezweef@hotmail.com>
2015-05-10 13:19:12 +0200

    Removed unnecessary bonenames

--- BoneInspector Rework/contour/BaseContour.cs
+++ BoneInspector Rework/contour/BaseContour.cs
@@ -21,14 +21,6 @@ namespace BoneInspector_Rework.contour
 
         private PointF last_p;
 
-        //public enum HandBones
-        //{
-        //    MC1, MC2, MC3, MC4, MC5,
-        //    PP1, PP2, PP3, PP4, PP5,
-        //    MP1, MP2, MP3, MP4, MP5,
-        //    DP1, DP2, DP3, DP4, DP5
-        //}
-
         public BaseContour()
         {
             drawnPoints = new List<PointF>();
